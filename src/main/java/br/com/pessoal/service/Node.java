628f87787044c6026b518a7054c190c9c9b44434
Felipe <felipe.ferreira1003@gmail.com>
2021-01-23 00:59:25 -0300

    Created static method valoresDaArvore who return a List with all values from Tree

--- src/main/java/br/com/pessoal/service/Node.java
+++ src/main/java/br/com/pessoal/service/Node.java
@@ -1,36 +1,52 @@
 package br.com.pessoal.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
 
 	private Integer valor;
 	private Node direita;
 	private Node esquerda;
-	
-	public static void add(Integer valor, Node raiz) {
-		if(raiz == null) {
-			raiz = new Node();
-			raiz.valor = valor;
-			return;
-		}
-		
-		if(valor > raiz.valor) {
-			if(raiz.direita == null) {
-				raiz.direita = new Node();
-				raiz.direita.valor = valor;
-				return;
-			}
-			Node.add(valor, raiz.direita);
-			return;
-		}
-		
-		if(raiz.esquerda == null) {
-			raiz.esquerda = new Node();
-			raiz.esquerda.valor = valor;
-			return;
-		}
-		Node.add(valor, raiz.esquerda);
+
+	public Node(Integer valor) {
+		this.valor = valor;
+		this.direita = this.esquerda = null;
 	}
-	
-	
-	
+
+	public static Node add(Integer valor, Node raiz) {
+		if (raiz == null) {
+			return new Node(valor);
+		}
+
+		Node galhos;
+
+		if (valor >= raiz.valor) {
+			galhos = new Node(valor);
+			raiz.direita = galhos;
+		}
+
+		if (valor <= raiz.valor) {
+			galhos = new Node(valor);
+			raiz.esquerda = galhos;
+		}
+
+		return raiz;
+
+	}
+
+	public static List<Integer> valoresDaArvore(Node no) {
+		List<Integer> listaArvore = new ArrayList<>();
+
+		if(no.esquerda != null)
+			Node.valoresDaArvore(no.esquerda);
+
+		listaArvore.add(no.valor);
+		
+		if(no.direita != null)
+			Node.valoresDaArvore(no.direita);
+		
+		return listaArvore;
+	}
+
 }
--- src/test/java/br/com/pessoal/service/test/ArvoreBinariaTest.java
+++ src/test/java/br/com/pessoal/service/test/ArvoreBinariaTest.java
@@ -1,14 +1,11 @@
 package br.com.pessoal.service.test;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import com.github.javafaker.Faker;
@@ -19,20 +16,21 @@ public class ArvoreBinariaTest {
 
 	@Test
 	public void deveRetornarOsValoresDaArvore(){
-		Node raiz = new Node();
+		Node raiz = null;
 		Faker faker = new Faker();
 		List<Integer> listaArvore = new ArrayList<>();
 		
 		for(int i = 0 ; i < 10 ; i++ ){
 			listaArvore.add(faker.number().randomDigitNotZero());
-			Node.add(listaArvore.get(listaArvore.size() - 1), raiz);
+			raiz = Node.add(listaArvore.get(listaArvore.size() - 1), raiz);
 		}
 		
-		List<Integer> listaRaiz = raiz.valoresDaArvore();
+		List<Integer> listaRaiz = Node.valoresDaArvore(raiz);
 		
 		for(int no : listaRaiz)
 			assertThat(listaRaiz, containsInAnyOrder(no));
 			
+		System.out.println(listaRaiz);
 	}
 	
 }
