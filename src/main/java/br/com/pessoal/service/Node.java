fea8ae3f0f47058615f7fa5ceaf0a13fb09d49cf
Felipe <felipe.ferreira1003@gmail.com>
2021-01-23 19:28:37 -0300

    Refacted deveRetornarOsValoresDaArvore test and Refact all Node class to test pass

--- src/main/java/br/com/pessoal/service/Node.java
+++ src/main/java/br/com/pessoal/service/Node.java
@@ -1,6 +1,5 @@
 package br.com.pessoal.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
@@ -11,41 +10,46 @@ public class Node {
 
 	public Node(Integer valor) {
 		this.valor = valor;
-		this.direita = this.esquerda = null;
+		direita = esquerda = null;
+	}
+	
+	public Node() {}
+	
+	public void add(Integer valor) {
+		if(this.valor == null) {
+			this.valor = valor;
+			return;
+		}
+		
+		if (valor <= this.valor) {
+			if (this.esquerda == null) {
+				this.esquerda = new Node(valor);
+				return;
+			}
+			this.esquerda.add(valor);
+			return;
+		}
+		
+		if(valor >= this.valor) {
+			if(this.direita == null) {
+				this.direita = new Node(valor);
+				return;
+			}
+			this.direita.add(valor);
+			return;
+		}
 	}
 
-	public static Node add(Integer valor, Node raiz) {
-		if (raiz == null) {
-			return new Node(valor);
-		}
-
-		Node galhos;
-
-		if (valor >= raiz.valor) {
-			galhos = new Node(valor);
-			raiz.direita = galhos;
-		}
-
-		if (valor <= raiz.valor) {
-			galhos = new Node(valor);
-			raiz.esquerda = galhos;
-		}
-
-		return raiz;
-
-	}
-
-	public static List<Integer> valoresDaArvore(Node no) {
-		List<Integer> listaArvore = new ArrayList<>();
-
-		if(no.esquerda != null)
-			Node.valoresDaArvore(no.esquerda);
+	public static List<Integer> arvorePreOrder(Node no, List<Integer> listaArvore) {
 
 		listaArvore.add(no.valor);
 		
-		if(no.direita != null)
-			Node.valoresDaArvore(no.direita);
-		
+		if (no.esquerda != null)
+			listaArvore = Node.arvorePreOrder(no.esquerda, listaArvore);
+
+		if (no.direita != null)
+			listaArvore = Node.arvorePreOrder(no.direita, listaArvore);
+
 		return listaArvore;
 	}
 
--- src/test/java/br/com/pessoal/service/test/ArvoreBinariaTest.java
+++ src/test/java/br/com/pessoal/service/test/ArvoreBinariaTest.java
@@ -1,7 +1,7 @@
 package br.com.pessoal.service.test;
 
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,22 +15,25 @@ import br.com.pessoal.service.Node;
 public class ArvoreBinariaTest {
 
 	@Test
-	public void deveRetornarOsValoresDaArvore(){
-		Node raiz = null;
+	public void deveRetornarOsValoresDaArvore() {
 		Faker faker = new Faker();
-		List<Integer> listaArvore = new ArrayList<>();
+		List<Integer> listaArvore = new ArrayList<Integer>();
+		Node raiz = new Node();
 		
-		for(int i = 0 ; i < 10 ; i++ ){
+		for (int i = 1; i < 10; i++) {
 			listaArvore.add(faker.number().randomDigitNotZero());
-			raiz = Node.add(listaArvore.get(listaArvore.size() - 1), raiz);
+			raiz.add(listaArvore.get(listaArvore.size()-1));
 		}
 		
-		List<Integer> listaRaiz = Node.valoresDaArvore(raiz);
+		System.out.println(listaArvore.size());
+		listaArvore.forEach(valor -> System.out.println(valor));
 		
-		for(int no : listaRaiz)
-			assertThat(listaRaiz, containsInAnyOrder(no));
-			
-		System.out.println(listaRaiz);
+		List<Integer> listaRaiz = Node.arvorePreOrder(raiz, new ArrayList<Integer>());
+		
+		for(Integer valor : listaRaiz) {
+			assertThat(listaArvore, hasItems(valor));
+		}
+
 	}
-	
+
 }
