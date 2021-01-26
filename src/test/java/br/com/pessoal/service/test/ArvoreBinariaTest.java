daf4954af13ea7fd0baddbe9ba2a3a0fa8ba4644
Felipe <felipe.ferreira1003@gmail.com>
2021-01-26 09:35:50 -0300

    Created a TDD method to test return from arvorePostOrder method in class Node

--- src/test/java/br/com/pessoal/service/test/ArvoreBinariaTest.java
+++ src/test/java/br/com/pessoal/service/test/ArvoreBinariaTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.junit.Test;
@@ -54,6 +55,35 @@ public class ArvoreBinariaTest {
 		List<Integer> listaBinariaOrganizada = Node.arvoreInOrder(raiz, new ArrayList<Integer>());
 		
 		for(Integer valores : listaBinariaOrganizada) {
+			assertThat(listaArvore, hasItems(valores));
+		}
+		
+	}
+	
+	
+	@Test
+	public void valoresDaArvoreEmOrdemDecrescente() {
+		
+		Faker faker = new Faker();
+		List<Integer> listaArvore = new ArrayList<>();
+		Node raiz = new Node();
+		
+		for(int i = 0; i < 10 ; i++) {
+			listaArvore.add(faker.number().randomDigitNotZero());
+			raiz.add(listaArvore.get(listaArvore.size() - 1));
+		}
+				
+		Collections.sort(listaArvore, new Comparator<Integer>() {
+
+			public int compare(Integer o1, Integer o2) {
+				if(o1 > o2)return -1;
+				if(o1 < o2)return 1;				
+				return 0;
+			}
+		});
+		List<Integer> listaBinariaDecrescente = Node.arvorePostOrder(raiz, new ArrayList<Integer>());
+		
+		for(Integer valores : listaBinariaDecrescente) {
 			assertThat(listaArvore, hasItems(valores));
 		}
 		
